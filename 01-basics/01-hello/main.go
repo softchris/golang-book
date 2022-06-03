9b56aa5f8446719d0e25ccc66831227eeaf52b29
softchris <noringc@gmail.com>
2022-06-03 16:01:40 +0100

    adding video

--- 01-basics/01-hello/main.go
+++ 01-basics/01-hello/main.go
@@ -4,5 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello")
-
+	fmt.Println("hey Chris")
+	var name = "Chris"
+	var age = 20
+	fmt.Printf("%s again, %d", name, age)
 }
---
+++ 01-basics/02-variables/exercise.go
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// defining variables
+// with and without type
+// the other way with :=
+
+// package main
+
+// import "fmt"
+
+// var (
+// 	name string
+// )
+
+// func main() {
+// 	name = "chris"
+// 	fmt.Println(name)
+// }
